13378e2ddd16ae97ddc6bfd05f06ecbefa936b9d
LucasSouza <82621071+LucaasSouza@users.noreply.github.com>
2023-11-01 16:39:41 -0300

    Alterando foto de perfil

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -37,11 +37,11 @@ export default function SideBar({ user }){
             >
                 <section className="text-center mt-4">
                     <Image
-                        // src={avatar_url}
                         width={200}
                         height={200}
                         alt="Foto de perfil"
-                        src="/img/user-default.png"
+                        src={avatar_url}
+                        // src="/img/user-default.png"
                         style={{ borderRadius: '100%' }}
                     />
 
